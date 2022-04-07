9658272665c76c527e84b6ca7012d19e61d9d8d6
ruan-seduc <103200540+ruan-seduc@users.noreply.github.com>
2022-04-07 18:42:11 -0300

    Add files via upload

---
+++ eeepSam/assets/js/main.js
@@ -0,0 +1,7 @@
+// Navbar collapsing
+$(".navbar-nav li a").click(function (event) {
+  var toggle = $(".navbar-toggle").is(":visible");
+  if (toggle) {
+    $(".navbar-collapse").collapse('hide');
+  }
+});
