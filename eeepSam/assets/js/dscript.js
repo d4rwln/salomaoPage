17b71c7c1b24f72d04429202fe741faa309c15d4
d4rwln <darwinop@gmail.com>
2022-04-20 10:14:04 -0300

    hidden menu

---
+++ eeepSam/assets/js/dscript.js
@@ -0,0 +1,9 @@
+function showContent(param){
+   console.log('teste2');
+   document.getElementById("aboutUs").style.display="none";
+   document.getElementById("works").style.display="none";
+   document.getElementById("entrance").style.display="none";
+   document.getElementById("aprovated").style.display="none";
+   document.getElementById(param).style.display="block";
+   
+}
